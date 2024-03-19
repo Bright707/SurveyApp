f63ae1892945366edf51c49129b55a4823ad9ccb
USER <USER@DESKTOP-100FUF0>
2024-03-19 05:12:59 -0700

    Add project files.

---
+++ SurveyApp.Dal/Migrations/20240214164230_secondmi.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SurveyApp.Dal.Migrations
+{
+    public partial class secondmi : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SurveyLinks",
+                columns: table => new
+                {
+                    SurveyLinkId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SurveyId = table.Column<int>(type: "int", nullable: false),
+                    Link = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SurveyLinks", x => x.SurveyLinkId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SurveyLinks");
+        }
+    }
+}
